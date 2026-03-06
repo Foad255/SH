c3b9451fb3d76500f1846b7393a209904ae6e06e
Fouad <f30071777@gmail.com>
2026-03-06 16:35:29 +0200

    cd builtIn_command

--- shell.c
+++ shell.c
@@ -9,10 +9,26 @@
 #define MAXLINE 4096
 #define MAXPROMPT 8191
 
+
+#define CLEAR_ARRAY(arr) do { \
+    for (size_t i = 0; i < sizeof(arr); i++) \
+        (arr)[i] = 0; \
+    } while(0)
+
 static void sig_int(int);
 
-void parseline(const char*cmdline,char**argv);
-char *get_prompt(char*pwd,char*home,char*prompt);
+void parseline(const char*cmdline,char**argv); 
+void set_prompt(char*pwd,char*home,char*prompt);
+int builtIn_command(char *arg);
+
+
+void cd(char *arg,char *pwd,char *home,char *prompt);
+
+/*
+cd dir (dir that is inside the working dir)
+cd /dir (absolute dir)
+cd ..   (one step back in in the path, if not root already)
+*/
 
 int 
 main(void)
@@ -20,13 +36,16 @@ main(void)
     char commandline[MAXLINE];
     char *args[MAXARGS];
     char prompt[MAXPROMPT];
+    char pwd[MAXPROMPT];
+    
     pid_t pid;
     int status;
-   
-    char * pwd  = getenv("PWD"); 
+ 
+
+    getcwd(pwd, MAXPROMPT);
     char * home = getenv("HOME");
 
-    get_prompt(pwd,home,prompt);
+    set_prompt(pwd,home,prompt);
   
 
     printf("%s %c ",prompt,'%');
@@ -34,15 +53,22 @@ main(void)
     while(fgets(commandline,MAXLINE,stdin) != NULL) {
        parseline(commandline,args);
 
-       if (!strcmp(args[0], "quit")) {
-            printf("Good bye\n");
-            exit(0);
-       }
 
+       int r = builtIn_command(args[0]);
+       switch(r) {
+        case 0:
+           exit(0);
+           break;
+        case 1:
+            CLEAR_ARRAY(pwd);
+            CLEAR_ARRAY(prompt);
+            cd(args[1],pwd,home,prompt);  
+            break;
+       } 
 
-       pid = fork();
-     
-       if (pid == 0) {
+       if((pid = fork()) < 0)
+           printf("Fork Error\n");
+       else if (pid == 0) {
         execvp(args[0],args);
         exit(127);
        }
@@ -79,13 +105,14 @@ parseline(const char *cmdline, char **argv)
     }
     argv[argc] = NULL;
   
-    
-    for (int i = 0; i < argc; i++) {
-        printf("argv[%d] = %s\n",i,argv[i]);
-    }
+    // 
+    // for (int i = 0; i < argc; i++) {
+    //     printf("argv[%d] = %s\n",i,argv[i]);
+    // }
 }
 
-char * get_prompt(char*pwd,char*home,char*prompt) {
+void
+set_prompt(char*pwd,char*home,char*prompt) {
  
     int atHome = 1;
     for(int i = 0; i < strlen(home); i++){
@@ -108,6 +135,29 @@ char * get_prompt(char*pwd,char*home,char*prompt) {
             prompt[i] = pwd[i];
         }
       }
+}
+
+
+
+int 
+builtIn_command(char *arg) {
+    if (!strcmp(arg,"cd")) {
+        return 1;
+    }
+
+       
+   if (!strcmp(arg, "quit")) {
+        printf("Good bye from shell\n");
+        return 0;
+       }
+   return -1;
+}
+
+void
+cd(char *arg,char *pwd,char *home,char *prompt) {
+    chdir(arg);
+    getcwd(pwd,MAXPROMPT);
+    set_prompt(pwd,home,prompt);  
 }
 
 
